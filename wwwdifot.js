6d2634867980cea8f8bed6d47242d54f09bf5dcb
bbpp122 <64087439+bbpp122@users.noreply.github.com>
2020-08-05 17:35:01 +0800

    1.1
    1.1

--- wwwdifot.js
+++ wwwdifot.js
@@ -1,8 +1,6 @@
-<script>
-    (function () {
-        var id = "2415689770463175004-7284";
-        document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-        (window.adbyunion = window.adbyunion || []).push(id);
-    })();
-</script>
-<script async defer src="https://www.68710266.com/o.js"></script>
+(function (){
+                        var id = "2415689770463175004-7284";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.68710266.com/o.js"></script>');
+})();
